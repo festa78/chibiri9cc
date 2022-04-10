958e85bb5d26b143cad5819f2c7d5811a3a8c3a5
festa78 <nonamehorses78@gmail.com>
2022-04-10 09:09:33 +0900

    Parse statement with plus and minus

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,69 @@ fn main() {
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
-    println!("  mov rax, {}", args[1]);
+
+    let statement = args[1].to_string();
+    let mut chars = statement.chars().peekable();
+    let first_number =
+        pop_next_number(&mut chars).expect(format!("Invalid statement {}", &statement).as_str());
+    println!("  mov rax, {}", first_number);
+
+    while let Some(ops) = pop_next_ops(&mut chars) {
+        match ops {
+            Operation::Plus => println!(
+                "  add rax, {}",
+                pop_next_number(&mut chars)
+                    .expect(format!("Invalid statement {}", &statement).as_str())
+                    .as_str()
+            ),
+            Operation::Minus => println!(
+                "  sub rax, {}",
+                pop_next_number(&mut chars)
+                    .expect(format!("Invalid statement {}", &statement).as_str())
+                    .as_str()
+            ),
+        };
+    }
     println!("  ret");
+}
+
+#[derive(PartialEq, Eq)]
+enum Operation {
+    Plus,
+    Minus,
+}
+
+fn pop_next_ops(chars: &mut std::iter::Peekable<std::str::Chars>) -> Option<Operation> {
+    if let Some(ops) = chars.next() {
+        if ops == '+' {
+            return Some(Operation::Plus);
+        } else if ops == '-' {
+            return Some(Operation::Minus);
+        }
+    }
+    None
+}
+
+fn pop_next_number(chars: &mut std::iter::Peekable<std::str::Chars>) -> Option<String> {
+    let mut optional = Some("".to_string());
+    while let Some(mut number) = optional {
+        match chars.peek() {
+            Some(c) => {
+                if c.is_digit(10) {
+                    number.push(chars.next().unwrap());
+                    optional = Some(number);
+                } else {
+                    return Some(number);
+                }
+            }
+            _ => {
+                if number.len() > 0 {
+                    return Some(number);
+                } else {
+                    optional = None;
+                }
+            }
+        };
+    }
+    None
 }
