985aa0a2d5589340fe32913fd0984b206870f82a
festa78 <nonamehorses78@gmail.com>
2022-05-02 09:42:40 +0900

    apply rust clippy and fmt

--- chibiri9cc/src/main.rs
+++ chibiri9cc/src/main.rs
@@ -2,7 +2,7 @@ use std::fmt;
 
 #[derive(Debug)]
 enum TokenError {
-    TKUnknown,
+    Unknown,
 }
 
 impl std::error::Error for TokenError {}
@@ -15,21 +15,25 @@ impl fmt::Display for TokenError {
 
 #[derive(Debug, PartialEq)]
 enum TokenKind {
-    TkReserved,   // symbol
-    TkNum,       // integer
-    TkEof,       // end of token
+    Reserved, // symbol
+    Num,      // integer
+    Eof,      // end of token
 }
 
 struct Token {
     kind: TokenKind,
     next: Option<Box<Token>>,
-    val: Option<i32>,
     str: Option<String>,
 }
 
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let result = write!(f, "{:?}: {:?}\n", &self.kind, self.str.as_ref().unwrap_or(&"None".to_string()));
+        let result = writeln!(
+            f,
+            "{:?}: {:?}",
+            &self.kind,
+            self.str.as_ref().unwrap_or(&"None".to_string())
+        );
         if let Some(next_token) = &self.next {
             return next_token.fmt(f);
         }
@@ -63,14 +67,14 @@ fn pop_if_number(chars: &mut std::iter::Peekable<std::str::Chars>) -> Option<Str
                 if c.is_digit(10) {
                     number.push(chars.next().unwrap());
                     optional = Some(number);
-                } else if number.len() > 0{
+                } else if !number.is_empty() {
                     return Some(number);
                 } else {
                     return None;
                 }
             }
             _ => {
-                if number.len() > 0 {
+                if !number.is_empty() {
                     return Some(number);
                 } else {
                     optional = None;
@@ -81,13 +85,13 @@ fn pop_if_number(chars: &mut std::iter::Peekable<std::str::Chars>) -> Option<Str
     None
 }
 
-
-fn tokenize(chars: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, impl std::error::Error> {
+fn tokenize(
+    chars: &mut std::iter::Peekable<std::str::Chars>,
+) -> Result<Token, impl std::error::Error> {
     if chars.clone().count() == 0 {
-        return Ok(Token{
-            kind: TokenKind::TkEof,
+        return Ok(Token {
+            kind: TokenKind::Eof,
             next: None,
-            val: None,
             str: None,
         });
     }
@@ -95,24 +99,22 @@ fn tokenize(chars: &mut std::iter::Peekable<std::str::Chars>) -> Result<Token, i
     pop_if_space(chars);
 
     if let Some(number) = pop_if_number(chars) {
-        return Ok(Token{
-            kind: TokenKind::TkNum,
+        return Ok(Token {
+            kind: TokenKind::Num,
             next: Some(Box::new(tokenize(chars).unwrap())),
-            val: Some(number.parse::<i32>().unwrap()),
             str: Some(number),
         });
     }
 
     if let Some(ops) = pop_if_ops(chars) {
-        return Ok(Token{
-            kind: TokenKind::TkReserved,
+        return Ok(Token {
+            kind: TokenKind::Reserved,
             next: Some(Box::new(tokenize(chars).unwrap())),
-            val: None,
             str: Some(ops),
         });
     }
 
-    Err(TokenError::TKUnknown)
+    Err(TokenError::Unknown)
 }
 
 fn main() {
@@ -130,7 +132,7 @@ fn main() {
     let statement = args[1].to_string();
     let mut chars = statement.chars().peekable();
     let token = tokenize(&mut chars).unwrap();
-    if &token.kind == &TokenKind::TkEof {
+    if token.kind == TokenKind::Eof {
         panic!("no token found");
     }
 
@@ -138,20 +140,41 @@ fn main() {
 
     let mut next_ops_token = *token.next.expect("Invalide statement");
 
-    while &next_ops_token.kind != &TokenKind::TkEof {
-        if next_ops_token.kind != TokenKind::TkReserved {
-            panic!("unknown token {}, expect ops", next_ops_token.str.expect("token should have str"));
+    while next_ops_token.kind != TokenKind::Eof {
+        if next_ops_token.kind != TokenKind::Reserved {
+            panic!(
+                "unknown token {}, expect ops",
+                next_ops_token.str.expect("token should have str")
+            );
         }
 
         let next_num_token = *next_ops_token.next.expect("Invalid statement");
-        if next_num_token.kind != TokenKind::TkNum {
-            panic!("unknown token {}, expect number", next_num_token.str.expect("token should have str"));
+        if next_num_token.kind != TokenKind::Num {
+            panic!(
+                "unknown token {}, expect number",
+                next_num_token.str.expect("token should have str")
+            );
         }
 
-        match next_ops_token.str.expect("token should have str").chars().next().unwrap() {
-            '+' => println!("  add rax, {}", next_num_token.str.expect("token should have str")),
-            '-' => println!("  sub rax, {}", next_num_token.str.expect("token should have str")),
-            _ => panic!("unsupported ops {}", next_num_token.str.expect("token should have str")),
+        match next_ops_token
+            .str
+            .expect("token should have str")
+            .chars()
+            .next()
+            .unwrap()
+        {
+            '+' => println!(
+                "  add rax, {}",
+                next_num_token.str.expect("token should have str")
+            ),
+            '-' => println!(
+                "  sub rax, {}",
+                next_num_token.str.expect("token should have str")
+            ),
+            _ => panic!(
+                "unsupported ops {}",
+                next_num_token.str.expect("token should have str")
+            ),
         }
 
         next_ops_token = *next_num_token.next.expect("Invalid statement");
