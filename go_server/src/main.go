86eff2e1b307906dffb319b17c7c92a6f1abc1bf
festa78 <nonamehorses78@gmail.com>
2022-12-08 17:04:44 +0900

    feat: compile code from goapi

--- go_server/src/main.go
+++ go_server/src/main.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World in go\n")
+func test(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World in go test\n")
 
 	resp, err := http.Get("http://nginx-srv/compilerapi/")
 	if err != nil {
@@ -25,7 +25,25 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(body))
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Post("http://nginx-srv/compilerapi/compile", "application/json", r.Body)
+	if err != nil {
+		fmt.Fprintf(w, fmt.Sprintf("fail on calling compilerapi: %s\n", err))
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(w, fmt.Sprintf("fail on reading output of compilerapi: %s\n", err))
+		return
+	}
+
+	fmt.Fprintf(w, "%s", string(body))
+}
+
 func main() {
+	// http.HandleFunc("/", test) // ハンドラを登録してウェブページを表示させる
 	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
 	http.ListenAndServe(":8080", nil)
 }
